b5c2b2c489ecb110c682d34996fec2db042f9d9b
CodeyArch <codeyarch123@gmail.com>
2022-02-02 13:19:36 +0000

    Search Engine tool input from file

--- src/main/kotlin/searchEngine.kt
+++ src/main/kotlin/searchEngine.kt
@@ -1,17 +1,22 @@
+@file:Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+
+import java.io.File
+
 // Dataset used will be first name, last name, optional email
-fun main() {
-    searcher()
+fun main(args: Array<String>) {
+    val dataFile = if ("--data" in args) {
+        args[args.indexOf("--data") + 1]
+    } else {
+        null
+    }
+    searcher(dataFile)
 }
-fun searcher() {
-    val listOfSearches = mutableListOf<MutableList<String>>()
+fun searcher(dataFile: String?) {
+    val listOfSearches = mutableListOf<String>()
     var searchEnd = false
-    println("Enter the number of people: ")
-    val searchCount = readLine()?.toInt() ?: 0
-    println("Enter all people: ")
-    repeat(searchCount) {
-        // Takes input and adds it to the list of data
-        val searchableInput = readLine()?.split(" ")
-        searchableInput?.toMutableList()?.let { listOfSearches.add(it) }
+    val searchableInput = File(dataFile).readLines()
+    for(i in searchableInput) {
+        listOfSearches.add(i)
     }
     while (!searchEnd) {
         // Endless loop for easy searching
@@ -37,16 +42,15 @@ fun searcher() {
             }
         }
     }
-
 }
-fun findPerson(listOfSearches: MutableList<MutableList<String>>) {
+fun findPerson(listOfSearches: MutableList<String>) {
     // Takes data and compares it to list to search for it. Linear search used at this stage
     println("")
     println("Enter a name or email to search all suitable people: ")
     val dataInput = readLine()
     val foundSearches = mutableListOf<String>()
     for (i in listOfSearches) {
-        val stringVers = i.joinToString().replace(",", "")
+        val stringVers = i.replace(",", "")
         if (stringVers.contains(dataInput.toString(), ignoreCase = true)) {
             foundSearches.add(stringVers)
         }
@@ -57,11 +61,11 @@ fun findPerson(listOfSearches: MutableList<MutableList<String>>) {
         }
     } else println("No matching people found. ")
 }
-fun printAll(listOfSearches: MutableList<MutableList<String>>) {
+fun printAll(listOfSearches: MutableList<String>) {
     // Prints every entry in list
     println("")
     println("=== List of people ===")
     for (i in listOfSearches) {
-        println(i.joinToString().replace(",", ""))
+        println(i.replace(",", ""))
     }
 }
