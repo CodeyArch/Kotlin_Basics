c679085bd0b56474eabf2479e72a44fac4b06b3c
CodeyArch <codeyarch123@gmail.com>
2022-02-12 15:56:39 +0000

    Steganography hidden message encryption and decryption

--- src/main/kotlin/Steganography.kt
+++ src/main/kotlin/Steganography.kt
@@ -3,9 +3,7 @@ import java.io.File
 import javax.imageio.ImageIO
 import java.awt.Color
 fun main() {
-    menu()
-}
-fun menu() {
+    // Menu to access hide and show
     var isOver = false
     while (!isOver) {
         println("Task (hide, show, exit):")
@@ -21,12 +19,15 @@ fun menu() {
     }
 }
 fun hide() {
+    // IO
     println("Input image file:")
     val inputFileName = readln()
     println("Output image file:")
     val outputFileName = readln()
     println("Message to hide:")
     val message = readln()
+    println("Password:")
+    val password = readln()
     val inputImage: BufferedImage
     try {
         inputImage = ImageIO.read(File(inputFileName))
@@ -38,7 +39,8 @@ fun hide() {
         println("The input image is not large enough to hold this message.")
         return
     } else {
-        val encodedMessage = message.encodeToByteArray() + byteArrayOf(0,0,3)
+        val newMessage = encodeOrDecodeMessage(password, message) // Encrypts message
+        val encodedMessage = newMessage.encodeToByteArray() + byteArrayOf(0,0,3) // To find message in image
         encodedMessage.forEachIndexed { idx, byte ->
             for (bitCount in (Byte.SIZE_BITS - 1) downTo 0) {
                 val bit = byte.toInt() shr bitCount and 1
@@ -49,6 +51,7 @@ fun hide() {
 
                 val color = Color(inputImage.getRGB(x, y))
                 val blue = changeLastBit(color.blue, bit)
+                // Hiding message in blue pixels
                 val colorNew = Color(
                     color.red,
                     color.green,
@@ -66,9 +69,35 @@ private fun bitsRead(idx: Int, bitCount: Int) =
 
 private fun changeLastBit(byte: Int, bit: Int) = byte shr 1 shl 1 or bit
 
+fun encodeOrDecodeMessage(password: String, message: String, decode: Boolean = false): String {
+    var newPassword = ""
+    // Validation to ensure password is same length as message
+    return if (password.length < message.length) {
+        while(newPassword.length < message.length) {
+            newPassword += password.repeat(1)
+        }
+        newPassword = newPassword.dropLast(newPassword.length - message.length)
+        if (decode) {
+            message xor newPassword
+        } else {
+            newPassword xor message
+        }
+
+    } else {
+        newPassword = password.dropLast(password.length - message.length)
+        if (decode) {
+            message xor newPassword
+        } else {
+            newPassword xor message
+        }
+    }
+}
 fun show () {
+    // IO
     println("Input image file:")
     val inputFileName = readln()
+    println("Password:")
+    val password = readln()
     val bufferedImage: BufferedImage
     try {
         bufferedImage = ImageIO.read(File(inputFileName))
@@ -79,13 +108,11 @@ fun show () {
     var message = byteArrayOf()
     var bitBuffer = 0
     var bitCount = Byte.SIZE_BITS - 1
+    // Finding message in image
     loop@for (y in 0 until bufferedImage.height) {
         for (x in 0 until bufferedImage.width) {
             val color = Color(bufferedImage.getRGB(x, y))
 
-            if(message.takeLast(3).containsAll(listOf(0,0,3))) {
-                break@loop
-            }
             val bit = color.blue and 1
             bitBuffer += bit shl bitCount
             bitCount--
@@ -94,8 +121,18 @@ fun show () {
                 bitBuffer = 0
                 bitCount = Byte.SIZE_BITS - 1
             }
+            if(message.takeLast(2).containsAll(listOf(0,0,3))) {
+                break@loop // Message found, stop searching
+            }
         }
     }
-    val fullmessage = message.dropLast(3).toByteArray().toString(Charsets.UTF_8)
-    println("Message: $fullmessage")
+    val fullMessage = message.dropLast(3).toByteArray().toString(Charsets.UTF_8) // Remove locator
+    val newFullMessage = encodeOrDecodeMessage(password, fullMessage, true) // Decode
+    println("Message: $newFullMessage")
+}
+// XOR algorithm
+infix fun String.xor(that: String) = mapIndexed { index, c ->
+    that[index].code.xor(c.code)
+}.joinToString(separator = "") {
+    it.toChar().toString()
 }
