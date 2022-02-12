585d22da1932b5be73e32818b7615a6bdcfd9723
CodeyArch <codeyarch123@gmail.com>
2022-02-12 09:15:05 +0000

    Steganography image handling

--- src/main/kotlin/Steganography.kt
+++ src/main/kotlin/Steganography.kt
@@ -1,3 +1,8 @@
+import java.io.File
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import java.awt.Color
+
 fun main() {
     menu()
 }
@@ -6,7 +11,7 @@ fun menu() {
     while (!isOver) {
         println("Task (hide, show, exit):")
         when (val menuInput = readln()) {
-            "hide" -> println("Hiding message in image.")
+            "hide" -> hide()
             "show" -> println("Obtaining message from image.")
             "exit" -> {
                 println("Bye!")
@@ -15,4 +20,39 @@ fun menu() {
             else -> println("Wrong task: $menuInput")
         }
     }
+}
+fun hide() {
+    println("Input image file:")
+    val inputFileName = readln()
+    println("Output image file:")
+    val outputFileName = readln()
+    val outputImage: BufferedImage
+    val inputImage: BufferedImage
+    val bufferedImage: BufferedImage
+    try {
+        bufferedImage = ImageIO.read(File(inputFileName))
+    } catch (e: Exception){
+        println("Can't read input file!")
+        return
+    }
+    println("Input Image: $inputFileName")
+    println("Output Image: $outputFileName")
+    inputImage = bufferedImage
+    outputImage = bufferedImage
+    for (i in 0 until bufferedImage.width) {
+        for (j in 0 until bufferedImage.height) {
+            val color = Color(inputImage.getRGB(i, j))
+            val rgb = Color(
+                setLeastSignificantBitToOne(color.red),
+                setLeastSignificantBitToOne(color.green),
+                setLeastSignificantBitToOne(color.blue)
+            ).rgb
+            outputImage.setRGB(i, j, rgb)
+        }
+    }
+    println("Image $outputFileName is saved.")
+    ImageIO.write(outputImage, "png", File(outputFileName))
+}
+fun setLeastSignificantBitToOne (pixel: Int): Int {
+    return if (pixel % 2 == 0) pixel + 1 else pixel
 }
